4fa382401770187abc90b86e6270f0cfa30a0af1
KevinChheang <kevin.chh89@gmail.com>
2018-11-29 03:08:14 +0000

    Implemented HTTP POST on Sign-up page

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { StockService } from '../stock.service';
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  
+  firstName: string = "";
+  lastName: string = "";
+  email: string = "";
+  password: string = "";
 
-  constructor() { }
+  constructor(public _api: StockService) {}
+  
+  onPostUserInfo() {
+    this._api.postUserInfo(this.firstName, this.lastName, this.email, this.password)
+    .subscribe(
+        data => {
+          console.log("POST request is successful", data);
+        });
+  }
+  
 
   ngOnInit() {
   }
--- src/app/stock.service.ts
+++ src/app/stock.service.ts
@@ -8,6 +8,11 @@ import { HttpClient } from '@angular/common/http';
 export class StockService {
   api_key: string = "VHK3U5FZN14NIS6L";
   
+  // firstName: string = "Tester";
+  // lastName: string = "Test";
+  // email: string = "test@testing.com";
+  // password: string = "testing";
+  
   constructor(public _http: HttpClient) { }
   
   getStockData(symbol) {
@@ -15,5 +20,29 @@ export class StockService {
     return this._http.get("https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=JPY&apikey=" + this.api_key);
   }
   
+  postUserInfo(firstName, lastName="Shane", email, password) {
+    return this._http.post("http://meanstack-2018-5-viramint-phortonssf.c9users.io:8080/api/AppUsers",
+      {
+        "firstName": firstName,
+        "lastName": lastName,
+        "email": email,
+        "password": password
+      });
+  }
+  
+    // this.httpClient.post("http://127.0.0.1:3000/customers",
+    //     {
+    //         "name": "Customer004",
+    //         "email": "customer004@email.com",
+    //         "tel": "0000252525"
+    //     })
+    //     .subscribe(
+    //         data => {
+    //             console.log("POST Request is successful ", data);
+    //         },
+    //         error => {
+    //             console.log("Error", error);
+    //         }
+    //     ); 
   
 }
